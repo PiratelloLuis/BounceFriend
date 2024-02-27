8d29702eadf72bba8f4d3a1ea9c3a9aa9166648f
André Luis <andre.piratello@hotmail.com>
2024-02-27 13:49:34 -0300

    XD
    artes atualizadas

--- Scripts/menu.cs
+++ Scripts/menu.cs
@@ -4,9 +4,10 @@ using Godot;
 
 public partial class menu : Control
 {
+
     public override void _Ready()
     {
-
+		GD.Print("Game Ready");
     }
 
     public override void _Process(double delta)
@@ -22,18 +23,5 @@ public partial class menu : Control
 	}
 	public void _on_name_pressed(){
 		OS.ShellOpen("https://github.com/PiratelloLuis");
-	}
-	public void _on_color_right_pressed(){
-		
-	}
-	public void _on_color_left_pressed(){
-
-	}
-
-	public void _on_eye_left_pressed(){
-
-	}
-	public void _on_eye_right_pressed(){
-
 	}
 }
