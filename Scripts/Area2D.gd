eeee95f3ac7a560471d901fc6a9f4e4645007ee8
André Luis <andre.piratello@hotmail.com>
2024-03-26 12:39:44 -0300

    xd

---
+++ Scripts/Area2D.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(_body: PhysicsBody2D):
+	entered = true
+
+func _on_body_exited(_body):
+	entered = false
+
+func _process(_delta):
+	if entered == true:
+		get_tree().change_scene_to_file("res://Scenes/end_game.tscn")
+	
+
